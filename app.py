d67a5050c9dd0b1c1c9e069e617865ed28845fc5
wailrhazouani-tech <wail.rhazouani@uit.ac.ma>
2026-05-06 19:13:20 +0100

    Add Railway deployment configuration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 
 app = Flask(__name__)
@@ -71,4 +72,5 @@ def elevator_pitch():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
